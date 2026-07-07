c3af52a2eea12cff37dbe044d5d0eacefd81ec1f
Amr1192 <amr.ahmed11922911@gmail.com>
2026-07-07 20:09:31 +0300

    test file updated

--- test.js
+++ test.js
@@ -1,13 +1,26 @@
 const express = require("express")
 const app = express()
+const router = express.Router()
+const fs = require("fs")
+const myTours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 const mongoose = require("mongoose")
 const mongoschema = new mongoose.Schema({
-    name: String,
+    name: {type: String, unique: true},
     price: Number
 })
 const db = "mongodb://localhost:27017/mytest"
-const connection = mongoose.connect(db).then(console.log("DB connected successfully"))
+const connection = mongoose.connect(db).then(()=>console.log("DB connected successfully"))
 
 const Tour = mongoose.model("Tour",mongoschema)
-Tour.create({name:"amr",price:5})
-Tour.find().then(console.log("no items found"))
+async function run(req,res) {
+    await Tour.deleteMany()
+    await Tour.create(myTours)
+    const Founded = await Tour.find(req.query).sort("-price")
+    console.log(Founded)
+    res.send(Founded)
+    
+}
+app.use("/", router);
+router.get("/",run)
+
+app.listen(3000, ()=> console.log("server running successfully"))
